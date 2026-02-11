f34d7c1ae10243886d32ebb8bae3c88b1302d314
LINA-LGGRAM\LINA <haemahmtea@gmail.com>
2026-02-11 11:53:12 +0900

    Initial settings gateway : kotlin + spring

---
+++ gateway/src/main/kotlin/gate/way/WayApplication.kt
@@ -0,0 +1,11 @@
+package gate.way
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WayApplication
+
+fun main(args: Array<String>) {
+	runApplication<WayApplication>(*args)
+}
---
+++ gateway/src/test/kotlin/gate/way/WayApplicationTests.kt
@@ -0,0 +1,13 @@
+package gate.way
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
