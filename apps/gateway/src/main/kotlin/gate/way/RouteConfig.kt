cc475aae5aeb1c7fc1e949b55b628cfad06425b3
LINA-LGGRAM\LINA <haemahmtea@gmail.com>
2026-02-22 00:22:19 +0900

    update

--- apps/gateway/src/main/kotlin/gate/way/RouteConfig.kt
+++ apps/gateway/src/main/kotlin/gate/way/RouteConfig.kt
@@ -13,13 +13,24 @@ class RouteConfig {
     @Bean
     fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes {
-            // 1. 프론트엔드 (나머지 모든 경로 /**)
+            // 1. 백엔드 API (가장 구체적인 경로를 우선 배치)
+//            route("backend_service") {
+//                path("/api/v1/**")
+//                filters {
+//                    stripPrefix(1) // /api/v1/user -> /user 로 변경
+//                    addRequestHeader("X-Gateway-Auth", "Authorized-By-Kotlin")
+//                }
+//                // 동무, Django인가 Spring인가? 일단 이름은 맞춰두겠다.
+//                uri("http://cloud-backend-svc.default.svc.cluster.local")
+//            }
+
+            // 2. 프론트엔드 (나머지 모든 경로 /**)
             route("frontend_route") {
                 path("/**")
                 uri("http://cloud-frontend-svc.default.svc.cluster.local")
             }
 
-            // 2. 상태 점검용 (문제의 구간)
+            // 3. 상태 점검용 (문제의 구간)
             route("health_check") {
                 path("/ping")
                 filters { setStatus(200) }
